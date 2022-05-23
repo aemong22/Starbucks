0822c29ef9425a06153962f6f82930550bb9586b
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-05-23 16:11:04 +0900

    slogan기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Banner from "./components/banner/Banner";
+import Banner from "./components/main/Banner";
 function App() {
     return (
         <Router>
